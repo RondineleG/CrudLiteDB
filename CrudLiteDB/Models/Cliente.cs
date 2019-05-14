7061fba1e1c03607cf77373e380c4215abb06069
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-05-14 10:56:33 -0300

    Criano novos campos e validando entradada de dados

--- CrudLiteDB/Models/Cliente.cs
+++ CrudLiteDB/Models/Cliente.cs
@@ -6,9 +6,15 @@ namespace CrudLiteDB.Models
     public class Cliente
     {
         public int ClienteId { get; set; }
-        public string Nome { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage = "O nome é obrigatório", AllowEmptyStrings = false)]
+        public string Nome { get; set; }         
         public string Sobrenome { get; set; }
         public string Email { get; set; }
+
+        [StringLength(11, MinimumLength = 11)]
+        [Required(ErrorMessage = "O Telefone é obrigatório", AllowEmptyStrings = false)]
         public string Telefone { get; set; }
         public DateTime DataCadastro => DateTime.Now;
 
--- CrudLiteDB/wwwroot/js/main.js
+++ CrudLiteDB/wwwroot/js/main.js
@@ -10,4 +10,13 @@
             campo.value = "";
         }
     }
- 
+
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
