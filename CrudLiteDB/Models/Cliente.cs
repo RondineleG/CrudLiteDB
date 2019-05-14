3f45fb4f42e37ca68a1154b75fb26d2ff71c1c60
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-05-13 23:55:09 -0300

    Adicionanco validacao nos campos

--- CrudLiteDB/Models/Cliente.cs
+++ CrudLiteDB/Models/Cliente.cs
@@ -6,10 +6,10 @@ namespace CrudLiteDB.Models
     public class Cliente
     {
         public int ClienteId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Nome é obrigatório", AllowEmptyStrings = false)]
         public string Nome { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O Telefone é obrigatório", AllowEmptyStrings = false)]
         public string Telefone { get; set; }
         public DateTime DataCadastro => DateTime.Now;
 
---
+++ CrudLiteDB/wwwroot/js/main.js
@@ -0,0 +1,13 @@
+﻿ 
+        function somenteNumeros(numero)
+        {
+        var regex = /[^0-9.]/;
+        regex.lastIndex = 0;
+        var campo = numero;
+        if (regex.test(campo.value))
+        {
+            alert("Digite Apenas Numeros");
+            campo.value = "";
+        }
+    }
+ 
